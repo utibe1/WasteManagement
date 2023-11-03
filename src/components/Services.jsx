80875c16249bcd53f739164cd5659f4d37961faa
utibe1 <utibefrank287@gmail.com>
2023-11-03 03:01:07 -0700

    reponsive

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -5,7 +5,7 @@ const Services = () => {
   return(
     <div className='container mx-auto'>
         <h1 className='text-center font-bold text-4xl mt-20 mb-10'>Services</h1>
-          <div className='block lg:grid lg:grid-cols-3 gap-2 lg:ms-14 ms-1'>
+          <div className='block lg:grid lg:grid-cols-3 gap-2 lg:ms-14 ms-0'>
             <div className='p-5 m-4 bg-white rounded-bl-[50px] rounded-tr-[50px] rounded-none custom-shadow w-full lg:w-80 border-2 border-[#FFB967] pb-10'>
               <div className='flex items-center mb-3 justify-between'>
                 <h2 className='text-xl font-medium'>
